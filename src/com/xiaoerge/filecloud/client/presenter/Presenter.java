ad843528631f7e32cd3536db7037c267aba9089b
xiaoerge <xiaoerge616@aol.com>
2015-04-18 12:22:59 -0400

    presenter

---
+++ src/com/xiaoerge/filecloud/client/presenter/Presenter.java
@@ -0,0 +1,10 @@
+package com.xiaoerge.filecloud.client.presenter;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+/**
+ * Created by xiaoerge on 4/18/15.
+ */
+public interface Presenter {
+    public abstract void go(final HasWidgets widgets);
+}
