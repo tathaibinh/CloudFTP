de772dbce14bbeb3fd024bd18ab264d4a9914dde
xiaoerge <xiaoerge616@aol.com>
2015-04-23 21:56:39 -0400

    using milisecond as random session id

--- src/com/xiaoerge/cloudftp/client/CloudFTP.java
+++ src/com/xiaoerge/cloudftp/client/CloudFTP.java
@@ -4,6 +4,7 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.user.client.Cookies;
+import com.google.gwt.user.client.Random;
 import com.google.gwt.user.client.rpc.*;
 import com.google.gwt.user.client.ui.*;
 
@@ -18,7 +19,9 @@ public class CloudFTP implements EntryPoint {
 
     public void onModuleLoad() {
 
-        Cookies.setCookie("CLOUDFTPSESSIONID", "CLOUDFTPSESSIONID", null, null, "/", false);
+        //todo use different id
+        double sid = System.currentTimeMillis() * Random.nextDouble();
+        Cookies.setCookie("CLOUDFTPSESSIONID", Double.toString(sid), null, null, "/", false);
 
         final AuthServiceAsync authServiceAsync = GWT.create(AuthService.class);
         final ShellServiceAsync shellServiceAsync = GWT.create(ShellService.class);
@@ -46,50 +49,5 @@ public class CloudFTP implements EntryPoint {
         HandlerManager handlerManager = new HandlerManager(null);
         AppController appViewer = new AppController(authServiceAsync, shellServiceAsync, handlerManager);
         appViewer.refresh(RootPanel.get("container"));
-
-//        AsyncCallback<String> callback = new AsyncCallback<String>() {
-//            @Override
-//            public void onFailure(Throwable caught) {
-//
-//            }
-//
-//            @Override
-//            public void onSuccess(String result) {
-//                if (result != null && !result.isEmpty()) {
-//                    signInStatus("Success", "alert alert-success");
-//                    logger.log(Level.SEVERE, "true");
-//                    ls();
-//                }
-//            }
-//        };
-//        authServiceAsync.authenticateSession(callback);
-    }
-
-    private void setCookie() {
-//        String sessionID = result.getSessionId();
-//        final int DURATION = 1000 * 60 * 60;
-//        Date expires = new Date(System.currentTimeMillis() + DURATION);
-//        Cookies.setCookie("sid", sessionID, expires);
-    }
-
-    private void ls() {
-//        AsyncCallback<Vector<FileEntry>> callback = new AsyncCallback<Vector<FileEntry>>() {
-//            public void onFailure(Throwable caught) {
-//                logger.log(Level.SEVERE, "ls error");
-//            }
-//
-//            public void onSuccess(Vector<FileEntry> result) {
-//                if (result != null) {
-//                    for (FileEntry fileEntry : result) {
-//                        Label label = new Label();
-//                        label.setText(fileEntry.getFileName());
-//                        label.setStyleName("list-group-item");
-//                        RootPanel.get("lsPanel").add(label);
-//                    }
-//                }
-//            }
-//        };
-//
-//        shellServiceAsync.ls(".", callback);
     }
 }
