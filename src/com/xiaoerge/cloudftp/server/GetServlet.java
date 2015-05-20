85b6c4fb4813e66164056cf83d928c341e6a2f98
xiaoerge <xiaoerge616@aol.com>
2015-05-20 19:09:21 -0400

    tmp zip trail

--- src/com/xiaoerge/cloudftp/server/GetServlet.java
+++ src/com/xiaoerge/cloudftp/server/GetServlet.java
@@ -8,6 +8,7 @@ import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.ZipParameters;
 import net.lingala.zip4j.util.Zip4jConstants;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
 import javax.servlet.ServletException;
@@ -16,8 +17,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Vector;
 import java.util.logging.Logger;
 
 /**
@@ -53,35 +56,92 @@ public class GetServlet extends HttpServlet {
                     resp.setHeader("Content-Disposition", "attachment; filename=" + fileName);
 
                     channelSftp.get(fileName, resp.getOutputStream());
-                }
-                else {
-                    resp.setHeader("Content-Disposition", "attachment; filename=" + fileName+".zip");
+                } else {
+                    resp.setHeader("Content-Disposition", "attachment; filename=" + fileName);
 
-                    BufferedInputStream bufferedInputStream = new BufferedInputStream(channelSftp.get(fileName));
+                    Vector<ChannelSftp.LsEntry> entries = channelSftp.ls(channelSftp.pwd());
+                    ChannelSftp.LsEntry entry = null;
+                    for (ChannelSftp.LsEntry e : entries) {
+                        if (e.getFilename().equals(fileName)) {
+                            entry = e;
+                            break;
+                        }
+                    }
 
-                    IOUtils.copy(bufferedInputStream, resp.getOutputStream());
+                    String tempPath = "/tmp";
+                    get(tempPath, entry);
 
-//                    try {
-//                        // Initiate ZipFile object with the path/name of the zip file.
-//                        ZipFile zipFile = new ZipFile(fileName + ".zip");
-//
-//                        ZipParameters parameters = new ZipParameters();
-//                        parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
-//                        parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
-//
-//                        zipFile.addStream(channelSftp.get(fileName), parameters);
-//
-//                    } catch (ZipException e) {
-//                        e.printStackTrace();
-//                    }
+                    try {
+                        // Initiate ZipFile object with the path/name of the zip file.
+                        String tempFile = tempPath+"/"+fileName;
+                        ZipFile zipFile = new ZipFile(tempFile+".zip");
+
+                        ZipParameters parameters = new ZipParameters();
+                        parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+                        parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+
+                        zipFile.addFolder(tempFile, parameters);
+
+                        String pwd = channelSftp.pwd();
+
+                        channelSftp.cd(tempPath);
+
+                        channelSftp.get(zipFile.getFile().getName(), resp.getOutputStream());
+                        channelSftp.cd(pwd);
+
+                        FileUtils.forceDelete(zipFile.getFile());
+
+                    } catch (ZipException e) {
+                        e.printStackTrace();
+                    }
                 }
 
             } catch (SftpException e) {
                 e.printStackTrace();
             }
-        }
-        else {
+        } else {
             throw new ServletException();
+        }
+    }
+
+    /*
+    * this block is from
+    * https://github.com/xiaoerge/File-UI/blob/master/src/fileui/GUI.java#L364
+    * */
+    private void get(String path, ChannelSftp.LsEntry entry) throws SftpException {
+
+        SessionProfile sessionProfile = SessionProfile.getInstance();
+        ChannelSftp channelSftp = sessionProfile.getChannelsftp();
+
+        if (!entry.getAttrs().isDir()) {
+            channelSftp.get(entry.getFilename(), channelSftp.lpwd());
+
+        } else if (!entry.getFilename().equals(".") && !entry.getFilename().equals("..")) {
+            //cd into the dir
+            channelSftp.cd(path + "/" + entry.getFilename());
+
+            File file = new File(channelSftp.lpwd() + "/" + entry.getFilename());
+            file.mkdir();
+
+            channelSftp.lcd(file.getAbsolutePath());
+
+            Vector vec = channelSftp.ls(channelSftp.pwd());
+
+            for (Object o : vec) {
+                if (o instanceof ChannelSftp.LsEntry) {
+                    ChannelSftp.LsEntry local_entry = (ChannelSftp.LsEntry) o;
+
+                    if (!local_entry.getFilename().equals(".") && !local_entry.getFilename().equals("..")) {
+                        get(channelSftp.pwd(), local_entry);
+                    }
+                }
+            }
+
+            channelSftp.cd(path);
+            channelSftp.lcd(file.getParent());
+
+        } else {
+            System.err.println("Error get");
         }
     }
 }
